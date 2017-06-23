62ab24d2de263ba033306cd8c7ee2fca013c6350
Steve McDonald <spacemansteve@gmail.com>
2017-06-23 14:22:53 -0400

    #16 support for lists of metrics records, improved test comparision of floating point values (#21)

--- adsmsg/tests/test_metrics_record.py
+++ adsmsg/tests/test_metrics_record.py
@@ -1,8 +1,9 @@
 
 import unittest
-from adsmsg.metrics_record import MetricsRecord
+from adsmsg.metrics_record import MetricsRecord, MetricsRecordList
 from datetime import datetime
 import time
+from __builtin__ import float
 
 class TestMsg(unittest.TestCase):
 
@@ -14,7 +15,7 @@ class TestMsg(unittest.TestCase):
 
 
     def test_simple_data(self):
-        """tests fields where output data that exactly matches input data, covers most fields"""
+        """tests fields where output data that exactly matches input data and data has simple structure"""
         metrics_data = {'bibcode': '1954PhRv...93..256R',
                         'an_citations': 0.15625,
                         'an_refereed_citations': 0.2,
@@ -30,22 +31,17 @@ class TestMsg(unittest.TestCase):
                         'refereed_citations': ["1954PhRv...93..257G", "1954PhRv...96..730K", "1955PhRv...98...79M",
                                                "1956RScI...27..809A", "1958PhRv..112..543S", "1961RScI...32..621H"],
                         'reference_num': 8,
-                        'rn_citations':  0.693503,
-                        'rn_citations_hist':
-                        {"1992": 0.6800104945559491, "1961": 0.6003174603174604,
-                         "1963": 0.6717460317460318, "1955": 0.3380952380952381}
+                        'rn_citations':  0.693503
                         }
-        map_fields = ("rn_citations_hist",)
+                        
         m = MetricsRecord(**metrics_data)
         for key in metrics_data:
-            #self.assertEqual(getattr(m, key), metrics_data[key])
-            if key not in map_fields:
-                self.assertAlmostEqual(getattr(m, key), metrics_data[key], msg='{} field failed'.format(key))
+            if isinstance(metrics_data[key], float):
+                self.assertAlmostEqual(getattr(m, key), metrics_data[key], 6)
             else:
-                sub_m = getattr(m, key)
-                for sub_key in metrics_data[key]:
-                    self.assertAlmostEqual(sub_m[sub_key], metrics_data[key][sub_key], msg='{} map field failed for key {}'.format(key, sub_key))
+                self.assertEqual(getattr(m, key), metrics_data[key])
 
+            
     def test_time(self):
         """protobuf time field requires conversion back to python datetime"""
         test_datetime = datetime.now()
@@ -57,6 +53,7 @@ class TestMsg(unittest.TestCase):
         m_datetime = datetime.strptime(m_iso, "%Y-%m-%dT%H:%M:%S.%fZ")
         self.assertEqual(test_datetime, m_datetime)
 
+
     def test_id(self):
         """id field is not serialized, when present it should not raise exception"""
         metrics_data = {'bibcode': '1954PhRv...93..256R',
@@ -66,8 +63,8 @@ class TestMsg(unittest.TestCase):
         self.assertEqual(None, m_id)
 
 
-    def test_rn_citation_data(self):
-        """rn_citation_data list is converted to protobuf citation_record list"""
+    def test_rn_dict_data(self):
+        """rn_citation_data stored as array of dicts, rn_citations_hist stored as dict"""
         metrics_data = {'bibcode': '1954PhRv...93..256R',
                         'rn_citation_data':
                         [{"ref_norm": 0.2, "pubyear": 1954, "auth_norm": 0.25,
@@ -75,7 +72,11 @@ class TestMsg(unittest.TestCase):
                          {"ref_norm": 0.07142857142857142, "pubyear": 1954, "auth_norm": 0.25,
                           "bibcode": "1954PhRv...96..730K", "cityear": 1954},
                          {"ref_norm": 0.06666666666666667, "pubyear": 1954, "auth_norm": 0.25,
-                          "bibcode": "1955PhRv...98...79M", "cityear": 1955}]}
+                          "bibcode": "1955PhRv...98...79M", "cityear": 1955}],
+                        'rn_citations_hist':
+                        {"1992": 0.6800104945559491, "1961": 0.6003174603174604,
+                         "1963": 0.6717460317460318, "1955": 0.3380952380952381}
+                        }
         m = MetricsRecord(**metrics_data)
 
         t = metrics_data['rn_citation_data']  # test data
@@ -83,7 +84,30 @@ class TestMsg(unittest.TestCase):
         self.assertEqual(len(t), len(p))
         for i in range(0, len(t)):
             for key in t[i]:
-                self.assertAlmostEqual(t[i][key], getattr(p[i], key), msg='rn_citation_data field {}'.format(key))
+                if isinstance(t[i][key], float):
+                    self.assertAlmostEqual(t[i][key], getattr(p[i], key), 6, msg='rn_citation_data field {}'.format(key))
+                else:
+                    self.assertEqual(t[i][key], getattr(p[i], key), msg='rn_citation_data field {}'.format(key))
+
+        
+        t = metrics_data['rn_citations_hist']  # test data
+        p = m.rn_citations_hist  # protobuf data
+        self.assertEqual(len(t), len(p))
+        for key in t:
+            self.assertAlmostEqual(t[key], p[key], 6, msg='rn_citations_hist field {}'.format(key))
+
+
+    def test_record_list(self):
+        """simple test for test MetricsRecordList"""
+        metrics_data1 = metrics_data = {'bibcode': '1954PhRv...93..256R', 'id': 1, 'refereed': True}
+        metrics_data2 = metrics_data = {'bibcode': '1954PhRv...93..256M', 'id': 2, 'refereed': False}
+        metrics_data3 = metrics_data = {'bibcode': '1954PhRv...93..256S', 'id': 2, 'refereed': False}
+        metrics_list = [metrics_data1, metrics_data2, metrics_data3]
+        m = MetricsRecordList(metrics_records=metrics_list)
+        self.assertEqual(len(metrics_list), len(m.metrics_records))
+        for i in range(0, len(metrics_list)):
+            self.assertEqual(metrics_list[i]['bibcode'], m.metrics_records[i].bibcode)
+            self.assertEqual(metrics_list[i]['refereed'], m.metrics_records[i].refereed)
 
 
 
