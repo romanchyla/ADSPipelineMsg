84bf4a0ffe5f35b75dd81dd079ab812a38d5871c
Sergi Blanco-Cuaresma <marblestation@users.noreply.github.com>
2017-07-17 13:52:41 +0000

    Added fulltext update message to inform master of a fulltext update

--- adsmsg/__init__.py
+++ adsmsg/__init__.py
@@ -2,3 +2,4 @@ from .bibrecord import BibRecord
 from .denormalized_record import DenormalizedRecord
 from .orcid_claims import OrcidClaims
 from .fulltext_requests import FulltextRequests
+from .fulltext_update import FulltextUpdate
