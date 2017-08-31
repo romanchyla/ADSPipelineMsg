505f74c81b67c2fe8ce594567841ded9932090dc
Roman Chyla <roman.chyla@gmail.com>
2017-08-31 10:51:07 -0400

    Include the setuptools version in the hope it will force pip to re-build the correct dependency tree

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(name='adsmsg',
       version='0.0.1',
       packages=['adsmsg', 'adsmsg.protobuf', 'adsmsg.tests'],
       install_requires=[
+          'setuptools==36.2.5',
           'protobuf==3.3.0',
       ]
   )
