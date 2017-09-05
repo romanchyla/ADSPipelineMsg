05d73697bf3192c083a7e3c70f02e24e356cc3b7
Roman Chyla <roman.chyla@gmail.com>
2017-09-05 17:33:02 -0400

    Testing setuptools hack

--- setup.py
+++ setup.py
@@ -10,5 +10,6 @@ setup(name='adsmsg',
       packages=['adsmsg', 'adsmsg.protobuf', 'adsmsg.tests'],
       install_requires=[
           'protobuf==3.3.0',
+          'setuptools==36.2.5',
       ]
   )
