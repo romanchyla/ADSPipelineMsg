e5c7380a7a67a9ade1977fea0ec7ccc84d7a7a63
Roman Chyla <romanchyla@users.noreply.github.com>
2017-09-25 14:56:05 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(name='adsmsg',
       version='0.0.1',
       packages=['adsmsg', 'adsmsg.protobuf', 'adsmsg.tests'],
       install_requires=[
-          'protobuf==3.3.0',
-          'setuptools>=36.2.5',
+          'protobuf==3.3.0'
       ]
   )
