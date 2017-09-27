1a480b70f53cae0a8405bd739a9ba0b3b33e0e76
SpacemanSteve <Stephen.McDonald@cfa.harvard.edu>
2017-09-27 11:34:34 -0400

    forgot to update init file, modified author
    for solr, does non bib need to provide number of authors or does all author info come from bib pipeline

--- adsmsg/__init__.py
+++ adsmsg/__init__.py
@@ -4,8 +4,8 @@ from .fulltext_requests import FulltextRequests
 from .fulltext_update import FulltextUpdate
 from .metrics_record import MetricsRecord
 from .metrics_record import MetricsRecordList
-from .nonbibrecord import NonBibRecord
-from .nonbibrecord import NonBibRecordList
+from .nonbibrecord import NonBibSolrRecord
+from .nonbibrecord import NonBibSolrRecordList
 from .orcid_claims import OrcidClaims
 from .status import Status
 from .citation_changes import CitationChanges, CitationChange
