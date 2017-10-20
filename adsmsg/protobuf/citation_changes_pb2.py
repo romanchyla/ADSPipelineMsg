781601886bda2dbe8a78a07baa973a0c744ac42d
Sergi Blanco-Cuaresma <marblestation@users.noreply.github.com>
2017-10-20 15:18:58 -0400

    Refactored citation changes protobuf (#38)

--- adsmsg/protobuf/citation_changes_pb2.py
+++ adsmsg/protobuf/citation_changes_pb2.py
@@ -14,15 +14,16 @@ _sym_db = _symbol_database.Default()
 
 
 import status_pb2 as status__pb2
+import citation_changes_content_type_pb2 as citation__changes__content__type__pb2
 
 
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='citation_changes.proto',
   package='adsmsg',
   syntax='proto3',
-  serialized_pb=_b('\n\x16\x63itation_changes.proto\x12\x06\x61\x64smsg\x1a\x0cstatus.proto\":\n\x0f\x43itationChanges\x12\'\n\x07\x63hanges\x18\x01 \x03(\x0b\x32\x16.adsmsg.CitationChange\"\x85\x01\n\x0e\x43itationChange\x12\x0e\n\x06\x63iting\x18\x01 \x01(\t\x12\r\n\x05\x63ited\x18\x02 \x01(\t\x12\x0b\n\x03\x64oi\x18\x03 \x01(\t\x12\x0b\n\x03pid\x18\x04 \x01(\t\x12\x0b\n\x03url\x18\x05 \x01(\t\x12\r\n\x05score\x18\x06 \x01(\t\x12\x1e\n\x06status\x18\x07 \x01(\x0e\x32\x0e.adsmsg.Statusb\x06proto3')
+  serialized_pb=_b('\n\x16\x63itation_changes.proto\x12\x06\x61\x64smsg\x1a\x0cstatus.proto\x1a#citation_changes_content_type.proto\":\n\x0f\x43itationChanges\x12\'\n\x07\x63hanges\x18\x01 \x03(\x0b\x32\x16.adsmsg.CitationChange\"\xab\x01\n\x0e\x43itationChange\x12\x0e\n\x06\x63iting\x18\x01 \x01(\t\x12\r\n\x05\x63ited\x18\x02 \x01(\t\x12\x37\n\x0c\x63ontent_type\x18\x03 \x01(\x0e\x32!.adsmsg.CitationChangeContentType\x12\x0f\n\x07\x63ontent\x18\x04 \x01(\t\x12\x10\n\x08resolved\x18\x05 \x01(\x08\x12\x1e\n\x06status\x18\x06 \x01(\x0e\x32\x0e.adsmsg.Statusb\x06proto3')
   ,
-  dependencies=[status__pb2.DESCRIPTOR,])
+  dependencies=[status__pb2.DESCRIPTOR,citation__changes__content__type__pb2.DESCRIPTOR,])
 
 
 
@@ -53,8 +54,8 @@ _CITATIONCHANGES = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=48,
-  serialized_end=106,
+  serialized_start=85,
+  serialized_end=143,
 )
 
 
@@ -80,36 +81,29 @@ _CITATIONCHANGE = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='doi', full_name='adsmsg.CitationChange.doi', index=2,
-      number=3, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=_b("").decode('utf-8'),
+      name='content_type', full_name='adsmsg.CitationChange.content_type', index=2,
+      number=3, type=14, cpp_type=8, label=1,
+      has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='pid', full_name='adsmsg.CitationChange.pid', index=3,
+      name='content', full_name='adsmsg.CitationChange.content', index=3,
       number=4, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=_b("").decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='url', full_name='adsmsg.CitationChange.url', index=4,
-      number=5, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=_b("").decode('utf-8'),
+      name='resolved', full_name='adsmsg.CitationChange.resolved', index=4,
+      number=5, type=8, cpp_type=7, label=1,
+      has_default_value=False, default_value=False,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='score', full_name='adsmsg.CitationChange.score', index=5,
-      number=6, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=_b("").decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    _descriptor.FieldDescriptor(
-      name='status', full_name='adsmsg.CitationChange.status', index=6,
-      number=7, type=14, cpp_type=8, label=1,
+      name='status', full_name='adsmsg.CitationChange.status', index=5,
+      number=6, type=14, cpp_type=8, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -126,11 +120,12 @@ _CITATIONCHANGE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=109,
-  serialized_end=242,
+  serialized_start=146,
+  serialized_end=317,
 )
 
 _CITATIONCHANGES.fields_by_name['changes'].message_type = _CITATIONCHANGE
+_CITATIONCHANGE.fields_by_name['content_type'].enum_type = citation__changes__content__type__pb2._CITATIONCHANGECONTENTTYPE
 _CITATIONCHANGE.fields_by_name['status'].enum_type = status__pb2._STATUS
 DESCRIPTOR.message_types_by_name['CitationChanges'] = _CITATIONCHANGES
 DESCRIPTOR.message_types_by_name['CitationChange'] = _CITATIONCHANGE
