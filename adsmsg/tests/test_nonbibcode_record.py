06d157c7b7ac9e2061a26d49b2061cb3f9f28928
golnazads <28757512+golnazads@users.noreply.github.com>
2018-01-02 13:57:12 -0500

    added DataLinksRecordList and also unittest

--- adsmsg/tests/test_nonbibcode_record.py
+++ adsmsg/tests/test_nonbibcode_record.py
@@ -2,7 +2,7 @@
 import unittest
 from adsmsg import msg
 
-from adsmsg.nonbibrecord import NonBibRecord
+from adsmsg.nonbibrecord import NonBibRecord, DataLinksRecord
 
 class TestMsg(unittest.TestCase):
 
@@ -13,7 +13,7 @@ class TestMsg(unittest.TestCase):
         unittest.TestCase.tearDown(self)
 
 
-    def test(self):
+    def test_nonbib(self):
         """test creation of protobuf 
 
         should include at least all fields listed AdsDataSqlSync:adsdata/run.py:nonbib_to_master_fields"""
@@ -61,6 +61,30 @@ class TestMsg(unittest.TestCase):
             self.assertEqual(m.data_links_rows[i].title, nonbib_data['data_links_rows'][i]['title'])
             self.assertEqual(m.data_links_rows[i].item_count, nonbib_data['data_links_rows'][i]['item_count'])
 
+
+    def test_datalinks(self):
+        """test creation of protobuf
+
+        should include at least all fields listed AdsDataSqlSync:adsdata/run.py:nonbib_to_master_fields"""
+        datalinks_data = {'bibcode': '2003ASPC..295..361M',
+                          'data_links_rows': [{'link_type': 'a', 'link_sub_type': 'b',
+                                               'url': ['http://a', 'http://b'],
+                                               'title': ['x', 'y'],
+                                               'item_count':1},
+                                              {'link_type': 'aa', 'link_sub_type': 'bb',
+                                               'url': ['http://aa', 'http://bb'],
+                                               'title': ['xx', 'yy'],
+                                               'item_count':2}]
+                          }
+        m = DataLinksRecord(**datalinks_data)
+        self.assertEqual(m.bibcode, datalinks_data['bibcode'])
+
+        for i in range(len(datalinks_data['data_links_rows'])):
+            self.assertEqual(m.data_links_rows[i].link_type, datalinks_data['data_links_rows'][i]['link_type'])
+            self.assertEqual(m.data_links_rows[i].link_sub_type, datalinks_data['data_links_rows'][i]['link_sub_type'])
+            self.assertEqual(m.data_links_rows[i].url, datalinks_data['data_links_rows'][i]['url'])
+            self.assertEqual(m.data_links_rows[i].title, datalinks_data['data_links_rows'][i]['title'])
+            self.assertEqual(m.data_links_rows[i].item_count, datalinks_data['data_links_rows'][i]['item_count'])
 
 
 if __name__ == '__main__':
